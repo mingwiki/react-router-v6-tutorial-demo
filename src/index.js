3cd421c2089aec0de07e1736eccef6ee74405813
mingwiki <mingwiki@gmail.com>
2022-04-07 17:06:17 +0800

    Index Routes

--- src/index.js
+++ src/index.js
@@ -17,6 +17,14 @@ render(
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route
