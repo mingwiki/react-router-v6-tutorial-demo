0fcfd4853c07cd7dbf662ad79f07c9180cdb4576
mingwiki <mingwiki@gmail.com>
2022-04-07 17:04:33 +0800

    Reading URL Params

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {
 import App from "./App";
 import Expenses from "./routes/expenses";
 import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 const rootElement = document.getElementById("root");
 render(
@@ -15,6 +16,9 @@ render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route
           path="*"
           element={
